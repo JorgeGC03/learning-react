23d2374ccdee597b10de4a9e6021f6a19c39981b
Jorge Gala Caballero <jgala@AddingTechnology.local>
2025-02-04 15:31:55 +0100

    Subo otra forma de ahcerlo

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -5,10 +5,35 @@ import { TwitterFollowCard } from './TwitterFollowCard';
 export function App () {
     return (
         <section className="App">
-            <TwitterFollowCard isFollowing userName="JorgeGC03" name="Jorge Gala Caballero" />
-            <TwitterFollowCard isFollowing userName="cesiouvas" name="Sergio Castillo Llorens" />
-            <TwitterFollowCard isFollowing userName="genius666" name="Adrían Genís Ramos" />
-            <TwitterFollowCard isFollowing userName="joanaparici" name="Joan Aparici Morant" />
+            <TwitterFollowCard 
+                isFollowing 
+                userName="JorgeGC03" 
+                name="Jorge Gala Caballero" />
+            
+            <TwitterFollowCard 
+                isFollowing 
+                userName="cesiouvas" 
+                name="Sergio Castillo Llorens" />
+            
+            <TwitterFollowCard 
+                isFollowing 
+                userName="genius666" 
+                name="Adrían Genís Ramos" />
+            
+            <TwitterFollowCard 
+                isFollowing 
+                userName="joanaparici" 
+                name="Joan Aparici Morant" />
+            
+            <TwitterFollowCard 
+                isFollowing 
+                userName="CarloSzMz" 
+                name="Carlos Sanz Muñoz" />
+            
+            <TwitterFollowCard 
+                isFollowing 
+                userName="CarlosTD20"
+                 name="Carlos Tornero Dauden" />
         </section>
     )
 }
