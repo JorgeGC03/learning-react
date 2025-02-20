847d6d75f5ae75adc7103420619cf0630d1e9981
Jorge Gala Caballero <jgala@AddingTechnology.local>
2025-02-20 09:46:29 +0100

    corrección de los componentes

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -5,35 +5,13 @@ import { TwitterFollowCard } from './TwitterFollowCard';
 export function App () {
     return (
         <section className="App">
-            <TwitterFollowCard 
-                isFollowing 
-                userName="JorgeGC03" 
-                name="Jorge Gala Caballero" />
+            <TwitterFollowCard userName="JorgeGC03" >
+                Jorge Gala Caballero
+            </TwitterFollowCard>
             
-            <TwitterFollowCard 
-                isFollowing 
-                userName="cesiouvas" 
-                name="Sergio Castillo Llorens" />
-            
-            <TwitterFollowCard 
-                isFollowing 
-                userName="genius666" 
-                name="Adrían Genís Ramos" />
-            
-            <TwitterFollowCard 
-                isFollowing 
-                userName="joanaparici" 
-                name="Joan Aparici Morant" />
-            
-            <TwitterFollowCard 
-                isFollowing 
-                userName="CarloSzMz" 
-                name="Carlos Sanz Muñoz" />
-            
-            <TwitterFollowCard 
-                isFollowing 
-                userName="CarlosTD20"
-                 name="Carlos Tornero Dauden" />
+            <TwitterFollowCard userName="CarlosTD20" >
+                Carlos Tornero Dauden
+            </TwitterFollowCard>
         </section>
     )
 }
